5297f34531dffe4c4ff572b8174d4e8d102de7ae
Anton Facenapalm <Anton Facenapalm>
2018-04-27 00:01:01 +0300

    Documentation

--- source/syntax.h
+++ source/syntax.h
@@ -41,34 +41,6 @@ private:
     void gen_label(LabelID label);
     void gen_jump(LabelID label, JumpType type);
 
-    // program -> `program` `{` descriptions operands `}`
-    // descriptions -> { type desctiption `;` }
-    // type -> `int` | `string` | `bool` | `real`
-    // desctiption -> variable { `,` variable }
-    // variable -> Identificator [ `=` constant ]
-    // constant -> ConstInt | ConstString | ConstBoolean | ConstReal
-    // operators -> { operator }
-    // operator -> `if` `(` expression `)` operator [ `else` operator ]
-    //           | `while` `(` expression `)` operator
-    //           | `do` operator `while` `(` expression `)` `;`
-    //           | `continue` `;`
-    //           | `break` `;`
-    //           | `read` `(` Identificator `)` `;`
-    //           | `write` `(` expression { `,` expression } `)` `;`
-    //           | `{` operators `}`
-    //           | expression `;`
-    // expression -> expression_or { `=` expression_or }
-    // expression_or -> expression_and { `or` expression_and }
-    // expression_and -> expression_cmp { `and` expression_cmp }
-    // expression_cmp -> expression_sum { comparer expression_sum }
-    // comparer -> `<` | `>` | `<=` | `>=` | `==` | `!=`
-    // expression_sum -> expression_mul { summator expression_mul }
-    // summator -> `+` | `-`
-    // expression_mul -> expression_un { multiplier expression_un }
-    // multiplier -> `*` | `/` | `%`
-    // expression_un -> unary_op expression_un | operand
-    // unary_op -> `+` | `-` | `not`
-    // operand -> constant | Identificator | `(` expression `)`
     void state_program();
     void state_descriptions();
     void state_description(ValueType type);
